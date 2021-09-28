0d1130736db65f087cf47d92cc7d1f7e0e080049
gabadinho <gabadinho@gmail.com>
2021-09-28 20:44:11 +0200

    fixed cbox test ioc

---
+++ iocs/cboxIOC/iocBoot/ioccbox/cbox.cmd
@@ -0,0 +1,11 @@
+# Micro-Epsilon CBox/2A laser-controller support
+
+# Configure asyn network device
+drvAsynIPPortConfigure("CBOXCTRL", "192.168.100.1:1024")
+
+# Configure CBox/2A driver to device
+cbox2aDriverConfigure("CBOX", "CBOXCTRL")
+
+# Load records
+dbLoadTemplate("cbox.substitutions")
+
--- iocs/cboxIOC/iocBoot/ioccbox/st.cmd
+++ iocs/cboxIOC/iocBoot/ioccbox/st.cmd
@@ -11,13 +11,9 @@ cd "${TOP}"
 dbLoadDatabase "dbd/cbox.dbd"
 cbox_registerRecordDeviceDriver pdbbase
 
-drvAsynSerialPortConfigure("SOCAT_PORT", "/dev/pts/3")
-# Commands to /dev/pts/4 get forwarded to /dev/pts/3
-
-cbox2aDriverConfigure("CBOX2A", "SOCAT_PORT")
-
-## Load record instances
-dbLoadRecords("${MECBOX2A}/db/microepsilon_cbox2a.template", "P=MICROEPSILON:,R=CBOX2A,PORT=CBOX2A,SCAN=Passive,ADDR=0,OMAX=0,IMAX=0")
-
 cd "${TOP}/iocBoot/${IOC}"
+
+##
+< cbox.cmd
+
 iocInit
