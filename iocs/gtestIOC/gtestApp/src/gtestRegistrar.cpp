61e8d720f10d02e67379f23a24ce61c05c739a23
gabadinho <gabadinho@gmail.com>
2021-09-29 07:13:20 +0200

    initial gtest phase

--- iocs/gtestIOC/gtestApp/src/gtestRegistrar.cpp
+++ iocs/gtestIOC/gtestApp/src/gtestRegistrar.cpp
@@ -1,9 +1,27 @@
 #include <cstdio>
 
+#include <epicsStdio.h>
 #include <epicsExport.h>
+#include <initHooks.h>
+#include <epicsExit.h>
+
+#include <gtest/gtest.h>
+
+void gtestHook(initHookState state) {
+    if (state == initHookAtIocBuild) {
+        epicsStdoutPrintf("gtestIOC: initializing GoogleTest\n");
+        ::testing::InitGoogleTest();
+    } else if (state == initHookAfterDatabaseRunning) {
+        epicsStdoutPrintf("gtestIOC: running GoogleTest tests\n");
+        RUN_ALL_TESTS();
+    } else if (state == initHookAfterIocRunning) {
+        epicsStdoutPrintf("gtestIOC: exiting...\n");
+        epicsExit(0);
+    }
+}
 
 static void gtestRegister(void) {
-    printf("gtestRegister\n");
+    initHookRegister(gtestHook);
 }
 
 extern "C" {
--- iocs/gtestIOC/gtestApp/src/gtestSuite1.cpp
+++ iocs/gtestIOC/gtestApp/src/gtestSuite1.cpp
@@ -2,4 +2,9 @@
 
 #include "mecbox2a.h"
 
+TEST(module_name, test_name) {
+    std::cout << "Hello world!" << std::endl;
+    // Google Test will also provide macros for assertions.
+    ASSERT_EQ(1+1, 2);
+}
 
